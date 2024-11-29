38940cd17d4a112e190dda19237e4363cee95fb8
Sam201918 <00001105970917sp@al.educacao.sp.gov.br>
2024-11-29 19:19:38 -0300

    adicionando projeto

---
+++ perguntas.js
@@ -0,0 +1,29 @@
+criaCartao(
+    'Tres espiãs demais ',
+    'Qual foi o primeiro vilão ?',
+    'Foi Sebastian saga'
+)
+
+criaCartao(
+    'O Clube das Winx',
+    'Quem foi a setima fada ?',
+    'Roxy a Fada dos Animais'
+)
+
+criaCartao(
+    'Super Choque',
+    'Como se chama o melhor amigo de super choque ?',
+    'O nome era Gear'
+)
+
+criaCartao(
+    'X-Men: Evolution',
+    'Qual é o mutante mais forte?',
+    'Jean Grey'
+)
+
+criaCartao(
+    'Scooby-Doo',
+    'Qual foi o primeiro live-action de Scooby-Doo?',
+    'Scooby-Doo ano de 2002',
+)
